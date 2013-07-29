b56a43d889ecf3a974e1cebe421fe917b0eb7fcb
Cody Jackson <sxenog@gmail.com>
2013-07-28 19:32:28 -0700

    Added thrust-decay as an attribute that thrust-lever accepts.
    I just felt decay may be a nice feature to have.

--- controller/js/directives/thrust-lever.js
+++ controller/js/directives/thrust-lever.js
@@ -1,4 +1,4 @@
-application.directive("thrustLever", function(){
+application.directive("thrustLever", ["$timeout", function($timeout){
 	return {
 		restrict: 'E',
 		template: '<div class="panel" style="position:relative"><div class="handle" style="position:absolute;left:0;"></div></div>',
@@ -7,6 +7,35 @@ application.directive("thrustLever", function(){
 			var panel = $(element);
 			var handle = panel.find('.handle');
 
+			function get_percentage_of_x_position_on_panel(x){
+				var maxX = panel.outerWidth()-handle.outerWidth();
+				return x/maxX;
+			}
+
+			function get_thrust_percentage(){
+				return get_percentage_of_x_position_on_panel(handle.offset().left);
+			}
+
+			function move_lever(percentage){
+				if(percentage < 0 || percentage > 1)
+					throw "percentage must be between 0.0 and 1.0."
+				var maxX = panel.outerWidth()-handle.outerWidth();
+				var x = maxX*percentage;
+				handle.offset({left:x, top:handle.outerWidth().top});
+			}
+
+			var isHandleHeld = false;
+			(function decay_thrust(){
+				if(attributes.decayRate && !isHandleHeld){
+					var newPercentage = get_thrust_percentage() - (attributes.decayRate)/250;
+					if(newPercentage < 0)
+						newPercentage = 0;
+
+					move_lever(newPercentage);
+				}
+				$timeout(decay_thrust, 4, false);
+			})();
+				
 			function update_lever_position(mouseRelativeX){
 				var handleHalfWidth = handle.outerWidth()/2.0;
 				var percentage = (mouseRelativeX-handleHalfWidth)/panel.outerWidth();
@@ -20,22 +49,21 @@ application.directive("thrustLever", function(){
 				handle.offset({left:left, top:handlePosition.top});
 			}
 
-			var isHolding = false;
 			element.mouseenter(function(ev){
-				if(isHolding && !ev.which)
-					isHolding = false;
+				if(isHandleHeld && !ev.which)
+					isHandleHeld = false;
 			});
 			element.mousedown(function(ev){
-				isHolding = true;
+				isHandleHeld = true;
 				update_lever_position(ev.clientX);
 			});
 			element.mousemove(function(ev){
-				if(isHolding)
+				if(isHandleHeld)
 					update_lever_position(ev.clientX);
 			});
 			element.mouseup(function(){
-				isHolding = false;
+				isHandleHeld = false;
 			});
 		}
 	};
-});
+}]);
